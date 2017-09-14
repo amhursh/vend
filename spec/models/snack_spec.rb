91ef9224e62af74c799350715a1ba1432d5a9c89
amhursh <amhursh@gmail.com>
2017-09-14 10:57:39 -0600

    Add model testing for snack

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Snack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".price_converted" do
+		it "returns formatted price of snack" do
+			snack = Snack.create(name: "Cocacola", price: 2.68)
+			snack2 = Snack.create(name: "Pepsi", price: 1.5)
+			snack3 = Snack.create(name: "Bitcoin", price: 0.94093849384024)
+
+			expect(snack.price_converted).to eq "$2.68"
+			expect(snack2.price_converted).to eq "$1.50"
+			expect(snack3.price_converted).to eq "$0.94"
+		end
+	end
 end
